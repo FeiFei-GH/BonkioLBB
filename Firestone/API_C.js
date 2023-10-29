07937e8977ccd20a6641e771b79d86e266d979ed
CharSong24 <113944172+CharSong24@users.noreply.github.com>
2023-10-28 23:21:00 -0400

    Update API_C.js

--- Firestone/API_C.js
+++ Firestone/API_C.js
@@ -5,49 +5,31 @@
 // @description  Connects Bonk.io to Firebase Firestore
 // @author       Char
 // @match        https://bonk.io/gameframe-release.html
-// @grant        GM_xmlhttpRequest
-// @grant        GM_setValue
-// @grant        GM_getValue
-// @grant        GM_deleteValue
-// @require      https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js
-// @require      https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js
+// @require      https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js
+// @require      https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js
+// @require      https://www.gstatic.com/firebasejs/10.5.2/firebase-analytics.js
 // ==/UserScript==
-
-/** @var GM_xmlhttpRequest */
 
 // unfinished so far
 
-const FIREBASE_SDK_URL = "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyDrwbPO5X8RNKxwhrz_gFRRUwRWNjLcW4k",
+    authDomain: "bonkio-leaderboard-mod.firebaseapp.com",
+    projectId: "bonkio-leaderboard-mod",
+    storageBucket: "bonkio-leaderboard-mod.appspot.com",
+    messagingSenderId: "488491769413",
+    appId: "1:488491769413:web:9fb95b61f9ac6be8a5cd2c",
+    measurementId: "G-FWD7TM9K06"
+};
 
-GM_xmlhttpRequest({
-    method: "GET",
-    url: FIREBASE_SDK_URL,
-    onload: (response) => {
-        // load Firebase SDK
-        eval(response.responseText);
-
-        // initialize
-        firebase.initializeApp({
-            apiKey: "YOUR_API_KEY",
-            authDomain: "YOUR_AUTH_DOMAIN",
-            databaseURL: "YOUR_DATABASE_URL",
-            projectId: "YOUR_PROJECT_ID",
-            storageBucket: "YOUR_STORAGE_BUCKET",
-            messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-            appId: "YOUR_APP_ID"
-        });
-
-
-        const db = firebase.firestore();
-
-    }
-});
-
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 //read data
 async function fetchData(documentId) {
-    const docRef = doc(db, "YOUR_COLLECTION_NAME", documentId);
+    const docRef = doc(db, "YOUR_COLLECTION_NAME", documentId); // change  "YOUR_COLLECTION_NAME" to specific assemble name
     const docSnap = await getDoc(docRef);
     if (docSnap.exists()) {
         console.log("Document data:", docSnap.data());
