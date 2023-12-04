138ee296142073ab6ee848132ad8ecb3d4730c40
BoZhi Deng <106386075+BoZhiDeng@users.noreply.github.com>
2023-12-04 03:49:42 -0500

    Update UI
    Optimize code structure by pulling out the listener functions form addKeyTable(), so addKeyTable() is now an init function to put UI components together.

--- UI/UI.js
+++ UI/UI.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         LBB_DB UI
-// @version      1.0
+// @version      1.0.48
 // @description  Adds a keytable to bonk.io
 // @author       BZD
 // @namespace    http://tampermonkey.net/
@@ -69,6 +69,40 @@ window.setKeyTableOpacity = (opacity) => {
     }
 };
 
+// Function to process input and update the latest input variable
+window.LBB_UI.receive_Inputs = (args) => {
+    var jsonStr = args.match(/\{.*\}/)[0];
+    var jsonObj = JSON.parse(jsonStr);
+    window.latestInput = jsonObj.i;
+    window.updateKeyStyles();
+    return args;
+};
+
+function dragStart(e, dragItem) {
+    if (e.target.id !== "opacity-slider") {
+        let startX = e.clientX;
+        let startY = e.clientY;
+        let startRight = parseInt(window.getComputedStyle(dragItem).right, 10);
+        let startBottom = parseInt(window.getComputedStyle(dragItem).bottom, 10);
+
+        function dragMove(e) {
+            let moveX = startX - e.clientX;
+            let moveY = startY - e.clientY;
+            dragItem.style.right = (startRight + moveX) + "px";
+            dragItem.style.bottom = (startBottom + moveY) + "px";
+        }
+
+        function dragEnd() {
+            document.removeEventListener('mousemove', dragMove);
+            document.removeEventListener('mouseup', dragEnd);
+        }
+
+        document.addEventListener('mousemove', dragMove);
+        document.addEventListener('mouseup', dragEnd);
+    }
+}
+
+// Function to add the key table to the DOM
 const addKeyTable = () => {
     let dragItem = document.createElement("div");
     dragItem.id = "drag-container";
@@ -102,11 +136,8 @@ const addKeyTable = () => {
             </tr>
         </tbody>`;
 
-    // Append the keyTable to the dragItem
     dragItem.appendChild(keyTable);
-    document.body.appendChild(dragItem);
 
-    // Add slider row outside the keyTable to avoid resizing issues
     let sliderRow = document.createElement("div");
     sliderRow.id = "slider-row";
     sliderRow.style.display = "none";
@@ -115,68 +146,24 @@ const addKeyTable = () => {
                style="width: 100%;" oninput="window.setKeyTableOpacity(this.value)">`;
     dragItem.appendChild(sliderRow);
 
-    // Add keydown event listener for Shift + S to toggle the slider
-    document.addEventListener('keydown', function(e) {
+    document.body.appendChild(dragItem);
+
+    function toggleSliderDisplay(e) {
         if (e.shiftKey && e.key === 'S') {
-            const sliderRow = document.getElementById("slider-row");
             sliderRow.style.display = sliderRow.style.display === 'none' ? 'block' : 'none';
         }
-    });
-
-    // Make the dragItem draggable
-   dragItem.onmousedown = function(e) {
-    // Only trigger if we're not clicking on the slider
-    if (e.target.id !== "opacity-slider") {
-        // Initial mouse positions
-        let startX = e.clientX;
-        let startY = e.clientY;
-        // Get the initial position of the dragItem
-        let startRight = parseInt(window.getComputedStyle(dragItem).right, 10);
-        let startBottom = parseInt(window.getComputedStyle(dragItem).bottom, 10);
-
-        // When moving the mouse, calculate the distance the mouse has moved
-        // and update the position of the dragItem
-        document.onmousemove = function(e) {
-            let moveX = startX - e.clientX;
-            let moveY = startY - e.clientY;
-            dragItem.style.right = (startRight + moveX) + "px";
-            dragItem.style.bottom = (startBottom + moveY) + "px";
-        };
-
-        // Once the mouse is lifted up, stop moving the dragItem
-        document.onmouseup = function() {
-            document.onmousemove = null;
-        };
     }
-};
 
-    // Initialize the key styles
+    document.addEventListener('keydown', toggleSliderDisplay);
+    dragItem.addEventListener('mousedown', (e) => dragStart(e, dragItem));
+
     window.updateKeyStyles();
 };
-
-
-// Function to set up the key table
-const setup = () => {
-    addKeyTable();
-    // The opacity control slider is now within the key table, created in addKeyTable()
-}
 
 // Ensure setup is called when the document is fully loaded
 if (document.readyState === "complete" || document.readyState === "interactive") {
-    setup();
+    addKeyTable();
 } else {
-    document.addEventListener("DOMContentLoaded", setup);
+    document.addEventListener("DOMContentLoaded", addKeyTable);
 }
 
-// Function to process input and update the latest input variable
-LBB_UI.receive_Inputs = (args) =>  {
-    // Extract the JSON part from the input string
-    var jsonStr = args.match(/\{.*\}/)[0];
-    // Parse the JSON string to an object
-    var jsonObj = JSON.parse(jsonStr);
-    // Update the latest input and refresh the key styles
-    window.latestInput = jsonObj.i;
-    window.updateKeyStyles();
-
-    return args;
-};
