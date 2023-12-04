2a1a8c6b6cb650aaa4b26e5d494a819a005e8916
BoZhi Deng <106386075+BoZhiDeng@users.noreply.github.com>
2023-12-04 14:29:50 -0500

    Update UI
    Add features:
    Save user preference(location, size, opacity)
    Hide and show UI by hotkey shif+h
    
    Optimization:
    Code structure
    Comment

--- UI/UI.js
+++ UI/UI.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LBB_DB UI
-// @version      1.1.48
-// @description  Adds a keytable to bonk.io
+// @version      1.2.48
+// @description  Add a customizable key table overlay to the bonk.io game
 // @author       BZD
 // @namespace    http://tampermonkey.net/
 // @license      MIT
@@ -10,88 +10,101 @@
 // @grant        none
 // ==/UserScript==
 
-// This project uses modifications code from [https://greasyfork.org/en/scripts/445890-bonk-keytable]
-// which is available under the MIT license.
+window.LBB_UI = {}; // Namespace for encapsulating the UI functions and variables
 
-window.LBB_UI = {};
-
+// Use 'strict' mode for safer code by managing silent errors
 'use strict';
 
-// Initial position and size of the key table
+// Constants defining the initial position and size of the key table overlay
 const left = "0";
 const top = "0";
 const width = "172px";
 const height = "100px";
 
-// Variable to store the latest input
+// Variable to track the most recent key input by the user
 window.latestInput = 0;
 
-// Function to update the style of a key based on the latest input
+// Updates the visual representation of keys based on user input
 window.keyStyle = (keyname) => {
-    // Define input values for each key
+    // Mapping of key names to their corresponding binary input value
     let inputValues = {
-        '←': 1,
-        '→': 2,
-        '↑': 4,
-        '↓': 8,
-        'Heavy': 16,
-        'Special': 32
+        '←': 1, '→': 2, '↑': 4, '↓': 8, 'Heavy': 16, 'Special': 32
     };
-    // Update the background color of the key based on whether it's pressed
-    if (window.latestInput & inputValues[keyname]) {
-        document.getElementById(keyname).style.backgroundColor = '#808080';
-    } else {
-        document.getElementById(keyname).style.backgroundColor = '#333333';
-    }
+    // Change the key's background color if it's currently pressed
+    let keyElement = document.getElementById(keyname);
+    keyElement.style.backgroundColor = window.latestInput & inputValues[keyname] ? '#808080' : '#333333';
 };
 
-// Function to update the styles of all keys
+// Refresh the styles for all keys on the UI
 window.updateKeyStyles = () => {
-    var keys = ['←', '↑', '→', '↓', 'Heavy', 'Special'];
-    keys.forEach(key => {
-        window.keyStyle(key);
-    });
+    let keys = ['←', '↑', '→', '↓', 'Heavy', 'Special'];
+    keys.forEach(window.keyStyle);
 };
 
-// Function to reset the background color of all keys to gray
+// Reset the background color of all keys to the default state
 window.keyTableReset = () => {
-    var keys = ['←', '↑', '→', '↓', 'Heavy', 'Special'];
+    let keys = ['←', '↑', '→', '↓', 'Heavy', 'Special'];
     keys.forEach(key => {
         document.getElementById(key).style.backgroundColor = '#333333';
     });
 };
 
-// Function to set the opacity of the entire UI
+// Adjust the UI's opacity based on user input
 window.setKeyTableOpacity = (opacity) => {
-    const dragContainer = document.getElementById("drag-container");
-    if (dragContainer) {
-        dragContainer.style.opacity = opacity;
-    }
+    let dragContainer = document.getElementById("drag-container");
+    dragContainer.style.opacity = opacity;
+    saveUISettings(); // Save the new setting to local storage
 };
 
-// Function to process input and update the latest input variable
+// Process input data and invoke style updates
 window.LBB_UI.receive_Inputs = (args) => {
-    var jsonStr = args.match(/\{.*\}/)[0];
-    var jsonObj = JSON.parse(jsonStr);
+    // Extract the input data from the argument string
+    let jsonObj = JSON.parse(args.match(/\{.*\}/)[0]);
     window.latestInput = jsonObj.i;
     window.updateKeyStyles();
     return args;
 };
 
-// Event listener for toggling the slider display
-document.addEventListener('keydown', (e) => {
-    if (e.shiftKey && e.key === 'S') {
-        toggleSliderDisplay();
-    }
-});
+// Save the current state of the UI settings to local storage
+function saveUISettings() {
+    let dragContainer = document.getElementById("drag-container");
+    let settings = {
+        width: dragContainer.style.width,
+        height: dragContainer.style.height,
+        opacity: dragContainer.style.opacity,
+        bottom: dragContainer.style.bottom,
+        right: dragContainer.style.right,
+        display: dragContainer.style.display
+    };
+    localStorage.setItem('LBB_UI_Settings', JSON.stringify(settings));
+}
 
-// Function to toggle the slider row display
+// Load the UI settings from local storage and apply them
+function loadUISettings() {
+    let settings = JSON.parse(localStorage.getItem('LBB_UI_Settings'));
+    if (settings) {
+        let dragContainer = document.getElementById("drag-container");
+        let opacitySlider = document.getElementById("opacity-slider");
+        // Apply the saved settings to the UI elements
+        Object.assign(dragContainer.style, settings);
+        // Set the slider's position to reflect the current opacity
+        if (opacitySlider) {
+            opacitySlider.value = settings.opacity;
+        }
+    }
+}
+
+// Toggle the visibility of the slider row and adjust the UI dimensions
 function toggleSliderDisplay() {
-    const sliderRow = document.getElementById("slider-row");
-    const keyTable = document.getElementById("bonk_keytable");
-    const isVisible = sliderRow.style.display !== 'none';
-    sliderRow.style.display = isVisible ? 'none' : 'block';
-    keyTable.style.height = isVisible ? "calc(100% - 30px)" : "calc(100% - 50px)"; // Adjust as needed
+    let dragContainer = document.getElementById("drag-container");
+    let sliderRow = document.getElementById("slider-row");
+    let keyTable = document.getElementById("bonk_keytable");
+    let isVisible = sliderRow.style.display !== 'none';
+    // Calculate and adjust the heights when showing or hiding the slider
+    keyTable.style.height = `calc(100% - ${isVisible ? '30px' : '100px'})`;
+    dragContainer.style.height = `calc(${dragContainer.style.height} ${isVisible ? '-' : '+'} 50px)`;
+    sliderRow.style.display = isVisible ? 'none' : 'block'; // Toggle display
+    saveUISettings(); // Persist the changes
 }
 
 // Function to handle the drag event
@@ -119,6 +132,7 @@ function dragMove(startX, startY, startRight, startBottom, dragItem, e) {
 // Function to stop the dragging
 function dragEnd(dragMoveFn) {
     document.removeEventListener('mousemove', dragMoveFn);
+    saveUISettings();
 }
 
 // Function to start resizing the UI
@@ -159,9 +173,73 @@ function resizeMove(e, startX, startY, startWidth, startHeight, dragItem) {
 function resizeEnd(resizeMoveFn) {
     document.removeEventListener('mousemove', resizeMoveFn);
     document.removeEventListener('mouseup', resizeEnd);
+    saveUISettings();
 }
 
-// Function to add the key table to the DOM
+// Function to toggle the UI's visibility
+function toggleUIDisplay() {
+    const dragContainer = document.getElementById("drag-container");
+    if (dragContainer) {
+        dragContainer.style.display = dragContainer.style.display === 'none' ? 'block' : 'none';
+        saveUISettings(); // Save the settings whenever the display changes
+    }
+}
+
+// Factory function to create the slider row for opacity control
+function createOpacityControl() {
+    // Create container for the opacity controls with initial styles
+    let sliderRow = document.createElement("div");
+    sliderRow.id = "slider-row";
+    sliderRow.style.display = "none"; // Set to hidden until toggled
+    sliderRow.style.marginTop = "10px"; // Space above the slider
+    sliderRow.style.backgroundColor = "#2d2d2d"; // Dark theme background
+    sliderRow.style.borderRadius = "5px"; // Smoothed corners for aesthetics
+    sliderRow.style.padding = "10px"; // Inner spacing around the contents
+    sliderRow.style.boxSizing = "border-box"; // Ensures padding is included in total width/height
+    sliderRow.style.position = "relative"; // Enables absolute positioning of child elements
+
+    // Add a title to the slider row for visual clarity
+    let sliderTitle = document.createElement("div");
+    sliderTitle.textContent = "UI";
+    sliderTitle.style.position = "absolute"; // Positioned relative to sliderRow
+    sliderTitle.style.top = "0"; // Align to the top of sliderRow
+    sliderTitle.style.left = "10px"; // Horizontal position within sliderRow
+    sliderTitle.style.fontSize = "12px"; // Text size for readability
+    sliderTitle.style.fontWeight = "bold"; // Make the title text bold
+    sliderTitle.style.color = "#fff"; // White text for contrast
+    sliderRow.appendChild(sliderTitle); // Insert the title into the slider container
+
+    // Create a label for the opacity slider for accessibility
+    let opacityLabel = document.createElement("label");
+    opacityLabel.textContent = "Opacity";
+    opacityLabel.setAttribute("for", "opacity-slider"); // Associate with the slider
+    opacityLabel.style.fontSize = "12px"; // Consistent font size with the title
+    opacityLabel.style.fontWeight = "bold"; // Emphasize the label text
+    opacityLabel.style.marginRight = "5px"; // Space between label and slider
+    opacityLabel.style.display = "inline-block"; // Allows margin-top adjustment
+    opacityLabel.style.color = "#fff"; // White color for visibility
+    opacityLabel.style.marginTop = "25px"; // Vertical alignment with the slider
+    sliderRow.appendChild(opacityLabel); // Add the label to the slider container
+
+    // Create the opacity slider input, configuring its range and appearance
+    let opacitySlider = document.createElement("input");
+    opacitySlider.type = "range"; // Slider type for range selection
+    opacitySlider.id = "opacity-slider"; // Unique identifier for the slider
+    opacitySlider.min = "0.1"; // Minimum opacity value
+    opacitySlider.max = "1"; // Maximum opacity value (fully opaque)
+    opacitySlider.step = "0.1"; // Incremental steps for opacity adjustment
+    opacitySlider.value = "1"; // Default value set to fully opaque
+    opacitySlider.style.width = "calc(100% - 60px)"; // Width adjusted for the label
+    opacitySlider.style.verticalAlign = "middle"; // Center align with the label text
+    opacitySlider.oninput = function () {
+        window.setKeyTableOpacity(this.value); // Update the UI opacity in real-time
+    };
+    sliderRow.appendChild(opacitySlider); // Place the slider into the slider container
+
+    return sliderRow; // Return the fully constructed slider row element
+}
+
+// Main function to construct and add the key table UI to the DOM
 const addKeyTable = () => {
     // Create the main container 'dragItem'
     let dragItem = document.createElement("div");
@@ -240,16 +318,9 @@ const addKeyTable = () => {
     // Append the keyTable to the dragItem
     dragItem.appendChild(keyTable);
 
-    // Create the slider row for opacity control
-    let sliderRow = document.createElement("div");
-    sliderRow.id = "slider-row";
-    sliderRow.style.display = "none"; // Initially hidden
-    sliderRow.innerHTML = `
-        <input type="range" id="opacity-slider" min="0.1" max="1" step="0.1" value="1"
-               style="width: 100%;" oninput="window.setKeyTableOpacity(this.value)">`;
-
-    // Append the sliderRow to the dragItem
-    dragItem.appendChild(sliderRow);
+    // Append the opacity control to the dragItem
+    let opacityControl = createOpacityControl();
+    dragItem.appendChild(opacityControl);
 
     // Append the dragItem to the body of the page
     document.body.appendChild(dragItem);
@@ -257,14 +328,37 @@ const addKeyTable = () => {
     // Initialize the key styles
     window.updateKeyStyles();
 
-    // Add event listeners for dragging and resizing
+    // Add event listeners for dragging
     dragItem.addEventListener('mousedown', (e) => dragStart(e, dragItem));
+
+    // Add event listeners for resizing
     resizeButton.addEventListener('mousedown', (e) => startResizing(e, dragItem));
+
+    // Event listener for toggling the slider display
+    document.addEventListener('keydown', (e) => {
+        if (e.shiftKey && e.key === 'O') {
+            toggleSliderDisplay();
+        }
+    });
+
+    // Event listener for toggling the UI display
+    document.addEventListener('keydown', (e) => {
+        if (e.shiftKey && e.key === 'H') {
+            toggleUIDisplay();
+        }
+    });
+
+    // Call loadUISettings when the script is loaded
+    document.addEventListener("DOMContentLoaded", loadUISettings);
 };
 
-// Ensure setup is called when the document is fully loaded
+// Initialization logic to set up the UI once the document is ready
 if (document.readyState === "complete" || document.readyState === "interactive") {
     addKeyTable();
+    loadUISettings(); // Immediately load settings if the document is ready
 } else {
-    document.addEventListener("DOMContentLoaded", addKeyTable);
+    document.addEventListener("DOMContentLoaded", () => {
+        addKeyTable();
+        loadUISettings(); // Load settings after DOM content has loaded
+    });
 }
