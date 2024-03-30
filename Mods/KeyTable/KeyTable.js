45803652f7024745ddaec0a7275a7a185367ef86
Clarifie <friendlyneighborhoodemailbot1@gmail.com>
2024-03-29 21:50:57 -0400

    Fix using color attribute

--- Mods/KeyTable/KeyTable.js
+++ Mods/KeyTable/KeyTable.js
@@ -32,7 +32,7 @@ window.keyStyle = (keyname) => {
     };
     // Change the key's background color if it's currently pressed
     let keyElement = document.getElementById(keyname);
-    keyElement.style.backgroundColor = window.latestInput & inputValues[keyname] ? bonkHUD.styleHold.buttonColorHover : bonkHUD.styleHold.buttonColor;
+    keyElement.style.backgroundColor = window.latestInput & inputValues[keyname] ? bonkHUD.styleHold.buttonColorHover.color : bonkHUD.styleHold.buttonColor.color;
 };
 
 // Refresh the styles for all keys on the UI
@@ -45,7 +45,7 @@ window.updateKeyStyles = () => {
 window.keyTableReset = () => {
     let keys = ['←', '↑', '→', '↓', 'Heavy', 'Special'];
     keys.forEach(key => {
-        document.getElementById(key).style.backgroundColor = bonkHUD.styleHold.buttonColor;
+        document.getElementById(key).style.backgroundColor = bonkHUD.styleHold.buttonColor.color;
     });
 };
 
